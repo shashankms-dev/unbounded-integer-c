ab770e9e842a447aa5396eaafcd70075ebb79f98
Shashank M Shanbhag <shashankmshanbhag@gmail.com>
2023-03-22 08:23:39 +0530

    Moved ub_print to ubint.c, solved memory-safety issues caused by returning local stack variables

--- ubint.c
+++ ubint.c
@@ -1,24 +1,41 @@
-#include "ubint.h"
+#include <stdio.h>
 #include <stdint.h>
 #include <stdarg.h>
 #include <stdlib.h>
+#include "ubint.h"
 
-ubint ub_constructor(uint8_t sign, uint64_t size, ...) {
-	ubint number;
-	number.blocks = (uint64_t *)malloc(sizeof(uint64_t[size]));
+ubint ub_constructor(bool sign, uint64_t nblocks, ...) {
+	ubint *number;
+	number->blocks = (uint64_t *)malloc(sizeof(uint64_t[nblocks]));
 
 	va_list blocks;
-	va_start(blocks, size);
+	va_start(blocks, nblocks);
 
-	for(uint64_t i = 0; i < size; i++) {
-		number.blocks[i] = va_arg(blocks, uint64_t);
+	for(uint64_t i = 0; i < nblocks; i++) {
+		number->blocks[i] = va_arg(blocks, uint64_t);
 	}
-	number.sign = sign;
-	number.size = size;
+	number->sign = sign;
+	number->nblocks = nblocks;
 
 	return number;
 }
 
 void ub_destructor(ubint *number) {
 	free(number->blocks);
+	free(number);
+}
+
+void ub_print(ubint *number) {
+	if(number->sign) {
+		printf("-");
+	}
+
+	for(uint64_t i = 0; i < number->nblocks; i++) {
+		if(number->blocks[i] != 0) {
+			printf("%llu", number->blocks[i]);
+		}
+		else {
+			printf("00000000000000000000");
+		}
+	}
 }
