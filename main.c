64cda1c96f99781512518576d89bb8210fbaf335
Shashank M Shanbhag <shashankmshanbhag@gmail.com>
2023-04-02 23:38:24 +0530

    Indentation level set to 4 in all files

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "ubint.h"
 
 int main(int argc, char *argv[]) {
-	if(argc < 2) {
-		printf("Usage: %s <large_number> \n", argv[0]);
-		return -1;
-	}
-	ubint *number = ub_hex_string(argv[1]);
-	ub_print(number);
-	printf("\n");
-	ub_destructor(number);
+    if(argc < 2) {
+        printf("Usage: %s <large_number> \n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    ubint *number = ub_hex_string(argv[1]);
+    
+    ub_print(number);
+    printf("\n");
+    ub_destructor(number);
 }
