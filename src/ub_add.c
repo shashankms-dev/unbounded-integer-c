8b10167077d2cac7a59b7a215ce1a9b087e9bdbd
Shashank M Shanbhag <shashankmshanbhag@gmail.com>
2023-04-06 10:28:05 +0530

    Changed ub_add() to work for all combinations of operands including signed

--- src/ub_add.c
+++ src/ub_add.c
@@ -2,38 +2,27 @@
 #include <stdlib.h>
 #include "ubint.h"
 
-// Assuming both positive or both negative operands for now, not handling signage only magnitude
-
 ubint *ub_add(ubint *num1, ubint *num2) {
-    ubint *res = malloc(sizeof(ubint));
-    uint64_t buffer;
-    uint64_t iter, iter1, iter2;
-    uint32_t carry = 0, val1, val2;
-
-    res->n_blocks = (num1->n_blocks > num2->n_blocks) ? (num1->n_blocks + 1) : (num2->n_blocks + 1);
-    res->blocks = malloc(sizeof(uint32_t[res->n_blocks]));
-    if(num1->sign == 0 && num2->sign == 0) {
-        res->sign = 0;
+    ubint *res;
+    int cmp;
+    
+    if(num1->sign != num2->sign) {
+        res = ub_abssubtract(num1, num2);
+        cmp = ub_abscompare(num1, num2);
+        
+        if(cmp == 1) {
+            res->sign = num1->sign;
+        }
+        else if(cmp == -1) {
+            res->sign = num2->sign;
+        }
+        else {
+            return res;
+        }
     }
     else {
-        res->sign = 1;
-    }
-
-    iter = res->n_blocks;
-    iter1 = num1->n_blocks;
-    iter2 = num2->n_blocks;
-
-    while(1) {
-        val1 = (iter1 > 0) ? num1->blocks[--iter1] : 0;
-        val2 = (iter2 > 0) ? num2->blocks[--iter2] : 0;
-        buffer = val1 + val2 + carry;
-        res->blocks[--iter] = (uint32_t) buffer;
-        carry = buffer >> 32;
-
-        if(iter1 == 0 && iter2 == 0) {
-            res->blocks[--iter] = (uint32_t) carry;
-            break;
-        }
+        res = ub_absadd(num1, num2);
+        res->sign = num1->sign;
     }
 
     return res;
