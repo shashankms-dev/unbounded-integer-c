dfee2b6c9f994455ce8c8cc498da57065a7f092d
Shashank M Shanbhag <shashankmshanbhag@gmail.com>
2023-04-06 10:38:24 +0530

    Resolved logic issue in ub_abscompare()

--- src/ub_abscompare.c
+++ src/ub_abscompare.c
@@ -5,13 +5,12 @@ int ub_abscompare(ubint *num1, ubint *num2) {
         return (num1->length > num2->length) ? 1 : -1;
     }
 
-    if(num1->blocks[0] > num2->blocks[0]) {
-        return 1;
+    for(uint64_t i = num1->length; i > 0; i--) {
+        i = i - 1;
+        if(num1->blocks[i] != num2->blocks[i]) {
+            return (num1->blocks[i] > num2->blocks[i]) ? 1 : -1;
+        }
     }
-    else if(num1->blocks[0] < num2->blocks[0]) {
-        return -1;
-    }
-    else {
-        return 0;
-    }
+
+    return 0;
 }
