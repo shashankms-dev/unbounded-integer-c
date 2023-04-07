e1b795dac65f84b2bc9e8492e9177ee076d6287f
Shashank M Shanbhag <shashankmshanbhag@gmail.com>
2023-04-07 10:01:14 +0530

    Added required headers to the ub_zero module

--- src/ub_zero.c
+++ src/ub_zero.c
@@ -1,10 +1,12 @@
+#include <stdlib.h>
+#include <stdint.h>
 #include "ubint.h"
 
 ubint *ub_zero(void) {
     ubint *zero = malloc(sizeof(ubint));
     zero->sign = 0;
     zero->length = 1;
-    zero->blocks = malloc(sizeof( uint32_t ));
+    zero->blocks = malloc(sizeof(uint32_t));
     zero->blocks[0] = 0;
 
     return zero;
