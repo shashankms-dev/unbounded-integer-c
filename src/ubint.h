88b02b41bdcb17e5d2db1b8faa097dbef713c1e3
Shashank M Shanbhag <shashankmshanbhag@gmail.com>
2023-04-07 10:03:09 +0530

    Added missing function declarations to ubint.h

--- src/ubint.h
+++ src/ubint.h
@@ -9,26 +9,37 @@ typedef struct unbounded_integer {
 	uint8_t sign;
 } ubint;
 
-ubint *ub_constructor(uint8_t, uint64_t, ...);
+ubint *ub_create(uint8_t, uint64_t, ...);
 
-void ub_destructor(ubint *);
+void ub_free(ubint *);
+
+
+void ub_print(ubint *);
+
 
 ubint *ub_string(char *);
 
 ubint *ub_hex_string(char *);
 
-void ub_print(ubint *);
+
+ubint *ub_add(ubint *, ubint *);
+
+ubint *ub_absadd(ubint *, ubint *);
+
+ubint *ub_subtract(ubint *, ubint *);
+
+ubint *ub_abssubtract(ubint *, ubint *);
+
+ubint *ub_multiply(ubint *, ubint *);
+
+ubint *ub_divide(ubint *, ubint *);
+
 
 int ub_compare(ubint *, ubint *);
 
 int ub_abscompare(ubint *, ubint *);
 
-ubint *ub_add(ubint *, ubint *);
 
-ubint *ub_subtract(ubint *, ubint *);
-
-ubint *ub_multiply(ubint *, ubint *);
-
-ubint *ub_divide(ubint *, ubint *);
+ubint *ub_zero(void);
 
 #endif
