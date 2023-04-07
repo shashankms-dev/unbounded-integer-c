dcabf01e46a3dfee8dd4ee5eedb91659987d4092
Shashank M Shanbhag <shashankmshanbhag@gmail.com>
2023-04-07 10:37:59 +0530

    Changed ub_print() to skip leading blocks of zeroes

--- src/ub_print.c
+++ src/ub_print.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+#include <stdint.h>
 #include "ubint.h"
 
 void ub_print(ubint *num) {
+    uint64_t i;
+
     if(num->sign)
         printf("-");
 
-    for(uint64_t i = 0; i < num->length; i++) {
+    for(i = 0; i < num->length; i++) {
+        if(num->blocks[i] != 0)
+            break;
+    }
+
+    for(; i < num->length; i++) {
         printf("%08x", num->blocks[i]);
     }
 }
