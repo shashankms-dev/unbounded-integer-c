af8a104468c61d8e5546aa311d396b46ee118c98
Shashank M Shanbhag <shashankmshanbhag@gmail.com>
2023-04-07 10:06:25 +0530

    New function added - ub_absadd()

---
+++ src/ub_absadd.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include "ubint.h"
+
+ubint *ub_absadd(ubint *num1, ubint *num2) {
+    ubint *res = malloc(sizeof(ubint));
+    uint64_t buffer;
+    uint64_t iter, iter1, iter2;
+    uint32_t carry = 0, block1, block2;
+
+    res->sign = 0;
+    res->length = (num1->length > num2->length) ? (num1->length + 1) : (num2->length + 1);
+    res->blocks = malloc(sizeof( uint32_t[res->length] ));
+
+    iter1 = num1->length;
+    iter2 = num2->length;
+    iter = res->length;
+
+    while(1) {
+        block1 = (iter1 > 0) ? num1->blocks[--iter1] : 0;
+        block2 = (iter2 > 0) ? num2->blocks[--iter2] : 0;
+
+        buffer = block1 + block2 + carry;
+        res->blocks[--iter] = (uint32_t) buffer;
+        carry = buffer >> 32;
+
+        if(iter1 == 0 && iter2 == 0) {
+            res->blocks[--iter] = (uint32_t) carry;
+            break;
+        }
+    }
+
+    return res;
+}
