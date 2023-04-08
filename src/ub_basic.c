4d331f5d83d9ea85fd27c66b496345e152472760
Shashank M Shanbhag <shashankmshanbhag@gmail.com>
2023-04-09 03:55:31 +0530

    Renamed ubint.c to ub_basic.c

---
+++ src/ub_basic.c
@@ -0,0 +1,38 @@
+#include <stdint.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "ubint.h"
+
+ubint *ub_create(uint8_t sign, uint64_t length, ...) {
+    ubint *num;
+    num = malloc(sizeof(ubint));
+    num->sign = sign;
+    num->length = length;
+    num->blocks = malloc(sizeof( uint32_t[num->length] ));
+
+    va_list blocks;
+    va_start(blocks, length);
+    
+    for(uint64_t i = 0; i < length; i++) {
+    	num->blocks[i] = va_arg(blocks, uint32_t);
+    }
+    
+    return num;
+}
+
+void ub_free(ubint *num) {
+    free(num->blocks);
+    free(num);
+}
+
+ubint *ub_clone(ubint *num) {
+    ubint *clone = malloc(sizeof(ubint));
+    clone->sign = num->sign;
+    clone->length = num->length;
+    clone->blocks = malloc(sizeof( uint32_t[clone->length] ));
+    for(uint64_t i = 0; i < num->length; i++) {
+        clone->blocks[i] = num->blocks[i];
+    }
+
+    return clone;
+}
