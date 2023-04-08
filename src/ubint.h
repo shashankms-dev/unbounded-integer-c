2f598d2ed7e76b6229626368b9c120eeb2adb80d
Shashank M Shanbhag <shashankmshanbhag@gmail.com>
2023-04-09 03:38:31 +0530

    Added required function declarations to ubint.h

--- src/ubint.h
+++ src/ubint.h
@@ -9,37 +9,48 @@ typedef struct unbounded_integer {
 	uint8_t sign;
 } ubint;
 
+
+// Basic functions
 ubint *ub_create(uint8_t, uint64_t, ...);
-
 void ub_free(ubint *);
-
-
+ubint *ub_clone(ubint *);
 void ub_print(ubint *);
 
 
+// Functions to create number from string
 ubint *ub_string(char *);
-
 ubint *ub_hex_string(char *);
 
 
+// Functions to perform arithmetic operations
 ubint *ub_add(ubint *, ubint *);
-
-ubint *ub_absadd(ubint *, ubint *);
-
+ubint *ub_abs_add(ubint *, ubint *);
 ubint *ub_subtract(ubint *, ubint *);
-
-ubint *ub_abssubtract(ubint *, ubint *);
-
+ubint *ub_abs_subtract(ubint *, ubint *);
 ubint *ub_multiply(ubint *, ubint *);
-
 ubint *ub_divide(ubint *, ubint *);
 
 
+// Functions to perform comparision
 int ub_compare(ubint *, ubint *);
+int ub_abs_compare(ubint *, ubint *);
 
-int ub_abscompare(ubint *, ubint *);
+
+// Functions to perform bitwise operations
+ubint *ub_and(ubint *, ubint *);
+ubint *ub_or(ubint *, ubint *);
+ubint *ub_not(ubint *);
+ubint *ub_xor(ubint *, ubint *);
+ubint *ub_xnor(ubint *, ubint *);
+ubint *ub_twos_compl(ubint *);
 
 
-ubint *ub_zero(void);
+// Functions to perform bitwise operations in place
+void ub_dand(ubint *, ubint *);
+void ub_dor(ubint *, ubint *);
+void ub_dnot(ubint *);
+void ub_dxor(ubint *, ubint *);
+void ub_dxnor(ubint *, ubint *);
+void ub_dtwos_compl(ubint *);
 
 #endif
